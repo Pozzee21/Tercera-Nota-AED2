b3ec483b4f111ff55361f60e7f15827cf5bb27c8
erik4 <erikaromina.damiani@gmail.com>
2016-10-09 07:03:55 +0200

    Add files via upload

---
+++ src/estructura/ITablaHuffman.java
@@ -0,0 +1,10 @@
+package estructura;
+
+public interface ITablaHuffman {
+
+	public Lista getTabla();
+	public int getTamaño();
+	
+}
+
+
---
+++ src/estructura/TablaHuffman.java
@@ -0,0 +1,29 @@
+package estructura;
+
+public class TablaHuffman implements ITablaHuffman{
+
+	//	Variables
+	
+	
+
+	//	Constructor
+	
+	//	Métodos
+	
+	
+	//	Geters and Seters
+	public TablaHuffman() {
+
+	}
+
+	public Lista getTabla() {
+		
+		return null;
+	}
+
+	public int getTamaño() {
+		
+		return 0;
+	}
+
+}
